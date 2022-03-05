4973691c400925b3c137f51e359c2d9a44f70cf3
SaurabhSahu01 <saurabhsahu794@gmail.com>
2022-03-05 09:01:13 +0530

    NAME2(2) - second solution with the mistakes highlighted

---
+++ NAME1(2).cpp
@@ -0,0 +1,46 @@
+/*
+  The same previous concept of frequency array will be used. But this time I would be concatenating the child strings and then run the query on that.
+*/
+
+#include <iostream>
+using namespace std;
+bool isPermutation(string, string);
+int main() {
+	int test,N;
+	string child, parent1, parent2, parent;
+	cin>>test;
+	while(test--){
+		cin>>parent1;
+		cin>>parent2;
+		parent = parent1 + parent2;
+		cin>>N;
+        string testChild = "";
+		while(N--){
+			cin>>child;
+			testChild += child;
+		}
+        if(isPermutation(parent, testChild)) cout<<"YES"<<endl;  // just concatenate all the child strings and then direct run the function to check
+        else cout<<"NO"<<endl;
+	}
+	return 0;
+}
+
+bool isPermutation(string parent, string child){
+    int array[26] = {0};
+    for(int i=0;i<parent.length();i++){
+        if(parent[i] >= 97 && parent[i] <= 125){
+            array[parent[i]-97]++;
+        }
+    }
+    for(int i=0;i<child.length();i++){
+        if(child[i] >= 97 && child[i] <= 125){
+            array[child[i] - 97]--;
+        }
+    }
+    for(int i=0;i<26;i++){
+        if(array[i] < 0){
+            return false;
+        }
+    }
+    return true;
+}
